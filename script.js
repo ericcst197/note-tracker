edeba48c045a4d8d38857d4b1f13634a4a3805b6
Eric Choo <ericchoo_0520@yahoo.com>
2021-12-07 17:10:44 +0800

    add local storage

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const notes = document.querySelector('#notes');
 const titleInput = form.elements.title;
 const noteInput = form.elements.note;
 
-function addNote(name ,note){
+// let noteArr = []
+let noteID = 1;
+function Note(id, title, content){
+  this.id = id;
+  this.title = title;
+  this.text = content;
+}
+
+function getDataFromStorage(){
+    return localStorage.getItem("note") ? JSON.parse(localStorage.getItem("note")) : [];
+}
+  
+function addNote(note){
     const newNote = document.createElement('div');
     const cardCol = document.createElement('div');
     const title = document.createElement('h5');
@@ -11,22 +23,23 @@ function addNote(name ,note){
     const btnDiv = document.createElement('div');
     const viewBtn = document.createElement('button');
     const delBtn = document.createElement('button');
-
+    
     //Content
     notes.append(cardCol);
     cardCol.append(newNote);
     cardCol.classList.add('col');
+    cardCol.setAttribute("data-id", note.id);
     newNote.append(title, content, btnDiv );
     newNote.classList.add('card', 'h-100')
-    title.append(name); 
+    title.append(note.title); 
     title.classList.add('card-title');
 
     //To show overview content in the card
-    if(note.length > 40) {
-        note = note.substring(0,40);
-        content.append(note + '...')
+    if(note.text.length > 40) {
+        note.text = note.text.substring(0,40);
+        content.append(note.text + '...')
     } else {
-        content.append(note)
+        content.append(note.text)
     };
     content.classList.add('card-text');
 
@@ -44,21 +57,31 @@ function addNote(name ,note){
 } 
 
 
-
-function delNote() {
+function delNote(e) {
     let del = this.parentNode.parentNode.parentNode
-    del.remove();
+    del.remove()
+    let divID = e.target.parentNode.parentNode.parentNode.dataset.id
+    let note = getDataFromStorage()
+    let newNoteList = note.filter(item =>{
+        return item.id !== parseInt(divID);
+    });
+    localStorage.setItem("note", JSON.stringify(newNoteList));
 }
+
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
-
+    
     if(!noteInput.value){
         alert('Please write some notes before adding.')
-    } else {
-        addNote(titleInput.value, noteInput.value);
-        titleInput.value = ''
-        noteInput.value = ''
-    }
-});
+    } 
+    let note = getDataFromStorage()
+    let noteItem = new Note(noteID, titleInput.value, noteInput.value);
+    noteID++
+    note.push(noteItem);
+    localStorage.setItem('note', JSON.stringify(note))
+    addNote(noteItem);
+    titleInput.value = ''
+    noteInput.value = ''
 
+});
